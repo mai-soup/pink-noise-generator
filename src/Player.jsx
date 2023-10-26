e1078edadf0e90d9eafab70bcb497cd5af1b47dd
Maijs Garais <mai-soup@mailfence.com>
2023-10-26 18:57:23 +0100

    add noise.js and button to toggle play state, remove react-h5-audio-player

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,21 +1,50 @@
-import AudioPlayer from 'react-h5-audio-player'
+import { useRef, useState } from 'react'
 import './playerstyles.css'
+import './noise'
 
 const Player = () => {
+  const audioContext = useRef(null)
+  const bufferSize = 4096
+  const [volume, setVolume] = useState(0.5)
+
+  const togglePlaying = () => {
+    if (audioContext.current === null) {
+      // Create AudioContext if it doesn't exist
+      audioContext.current = new (window.AudioContext ||
+        window.webkitAudioContext)()
+      const pinkNoiseNode = audioContext.current.createPinkNoise(
+        bufferSize,
+        volume
+      )
+      pinkNoiseNode.connect(audioContext.current.destination)
+      pinkNoiseNode.start()
+    } else {
+      // Stop audio and close the AudioContext
+      audioContext.current.close().then(() => {
+        audioContext.current = null
+      })
+    }
+  }
+
+  const handleVolumeChange = event => {
+    const newVolume = parseFloat(event.target.value)
+    setVolume(newVolume)
+    if (audioContext.current !== null) {
+      // If AudioContext is active, update volume in real-time
+      const pinkNoiseNode = audioContext.current.createPinkNoise(
+        bufferSize,
+        newVolume
+      )
+      pinkNoiseNode.connect(audioContext.current.destination)
+      pinkNoiseNode.start()
+    }
+  }
+
   return (
     <>
-      <div className='flex justify-between'>
-        <div>
-          <h3 className='text-2xl font-medium'>Pink Noise</h3>
-        </div>
+      <div>
+        <button onClick={togglePlaying}>Pause/play</button>
       </div>
-      <AudioPlayer
-        src={'src'}
-        showJumpControls={false}
-        customAdditionalControls={[]}
-        customProgressBarSection={[]}
-        layout='stacked-reverse'
-      />
     </>
   )
 }
