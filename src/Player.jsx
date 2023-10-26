a54dc39d341819158041e052725cc479aaec8905
Maijs Garais <mai-soup@mailfence.com>
2023-10-26 20:26:15 +0100

    add working and styled volume slider

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,46 +1,64 @@
-import { useRef, useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import './playerstyles.css'
 import './noise'
+import 'toolcool-range-slider'
 import PauseIcon from './PauseIcon.jsx'
 import PlayIcon from './PlayIcon.jsx'
 
 const Player = () => {
   const audioContext = useRef(null)
-  const bufferSize = 4096
+  const gainNode = useRef(null)
+  const sliderRef = useRef(null)
   const [volume, setVolume] = useState(0.5)
   const [isPlaying, setIsPlaying] = useState(false)
+
+  const handleVolumeChange = newVolume => {
+    setVolume(newVolume)
+    if (gainNode.current && audioContext.current) {
+      gainNode.current.gain.setValueAtTime(
+        newVolume,
+        audioContext.current.currentTime
+      )
+    }
+  }
+
+  useEffect(() => {
+    const slider = sliderRef.current
+
+    const onChange = evt => {
+      const newVolume = evt.detail.value
+      handleVolumeChange(newVolume)
+    }
+
+    slider?.addEventListener('change', onChange)
+
+    return () => {
+      slider?.removeEventListener('change', onChange)
+    }
+  }, [])
 
   const togglePlaying = () => {
     if (audioContext.current === null) {
       // Create AudioContext if it doesn't exist
       audioContext.current = new (window.AudioContext ||
         window.webkitAudioContext)()
-      const pinkNoiseNode = audioContext.current.createPinkNoise(
-        bufferSize,
-        volume
+      gainNode.current = audioContext.current.createGain()
+      const pinkNoiseNode = audioContext.current.createPinkNoise(4096)
+      pinkNoiseNode
+        .connect(gainNode.current)
+        .connect(audioContext.current.destination)
+
+      gainNode.current.gain.setValueAtTime(
+        volume,
+        audioContext.current.currentTime
       )
-      pinkNoiseNode.connect(audioContext.current.destination)
       setIsPlaying(true)
     } else {
       // Stop audio and close the AudioContext
       audioContext.current.close().then(() => {
         audioContext.current = null
+        setIsPlaying(false)
       })
-      setIsPlaying(false)
-    }
-  }
-
-  const handleVolumeChange = event => {
-    const newVolume = parseFloat(event.target.value)
-    setVolume(newVolume)
-    if (audioContext.current !== null) {
-      // If AudioContext is active, update volume in real-time
-      const pinkNoiseNode = audioContext.current.createPinkNoise(
-        bufferSize,
-        newVolume
-      )
-      pinkNoiseNode.connect(audioContext.current.destination)
-      pinkNoiseNode.start()
     }
   }
 
@@ -53,6 +71,17 @@ const Player = () => {
         >
           {isPlaying ? <PauseIcon /> : <PlayIcon />}
         </button>
+      </div>
+      <div className='mx-4 mt-4 flex items-center justify-center'>
+        <label className='mr-2'>Volume:</label>
+        <tc-range-slider
+          value={volume}
+          min='0'
+          max='1'
+          step='0.01'
+          ref={sliderRef}
+          slider-bg-fill='#e11d48'
+        />
       </div>
     </>
   )
